abf455ad3755bcd3b65ccd335fe60c212c733daf
Geraldo Grell <85137475+Jikjii@users.noreply.github.com>
2023-01-15 16:36:51 -0500

    feat: Error handlers improved

--- backend/src/controllers/notes.ts
+++ backend/src/controllers/notes.ts
@@ -1,4 +1,6 @@
 import { RequestHandler } from "express";
+import createHttpError from "http-errors";
+import mongoose from "mongoose";
 import NoteModel from "../models/note";
 
 // GET ALL NOTES
@@ -16,20 +18,42 @@ export const getNote: RequestHandler = async (req, res, next) => {
   const noteId = req.params.noteId;
 
   try {
+    if (!mongoose.isValidObjectId(noteId)) {
+        throw createHttpError(400, "Invalid note id")
+    }
+
     const note = await NoteModel.findById(noteId).exec();
+
+    if (!note) {
+        throw createHttpError(404, "Note not found")
+    }
+
     res.status(200).json(note);
   } catch (error) {
     next(error);
   }
 };
 
+interface CreateNoteBody {
+  title?: string;
+  text?: string;
+}
+
 // CREATE NOTE
-export const createNote: RequestHandler = async (req, res, next) => {
+export const createNote: RequestHandler<
+  unknown,
+  unknown,
+  CreateNoteBody
+> = async (req, res, next) => {
   // get values outside the body
   const title = req.body.title;
   const text = req.body.text;
 
   try {
+    if (!title) {
+      throw createHttpError(400, "Note must have a title");
+    }
+
     const newNote = await NoteModel.create({
       title: title,
       text: text,
