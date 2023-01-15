7f3529094a10fea04e9a1a9eedab7d3a60779bbb
Geraldo Grell <85137475+Jikjii@users.noreply.github.com>
2023-01-15 15:47:18 -0500

    feat: MongoDB connected - Express Sever running - Error handling added

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -1,11 +1,32 @@
 import "dotenv/config";
-import express from "express";
+import express, { NextFunction, Request, Response } from "express";
+import NoteModel from "./models/note";
 
 const app = express();
 
-app.get("/", (req, res) => {
-    res.send("Hello, World!");
-  });
+// async / await means it will take a second to pull the data from the database
 
+app.get("/", async (req, res, next) => {
+  try {
+    // throw Error("This is an example of an error");
+    const notes = await NoteModel.find().exec();
+    res.status(200).json(notes);
+  } catch (error) {
+    next(error);
+  }
+});
 
-export default app
+// error for endpoint not found
+app.use((req, res, next) => {
+    next(Error("Endpoint not found"))
+});
+
+// eslint-disable-next-line @typescript-eslint/no-unused-vars
+app.use((error: unknown, req: Request, res: Response, next: NextFunction) => {
+  console.error(error);
+  let errorMessage = "An unknown error occurred";
+  if (error instanceof Error) errorMessage = error.message;
+  res.status(500).json({ error: errorMessage });
+});
+
+export default app;
