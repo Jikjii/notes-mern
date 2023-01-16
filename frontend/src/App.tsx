3b2e94fe2603d5ddc462cc0f25b26bd35fb97cb8
Geraldo Grell <85137475+Jikjii@users.noreply.github.com>
2023-01-16 18:06:43 -0500

    feat: Created add note UI

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,19 +1,23 @@
 import React, { useEffect, useState } from "react";
 import logo from "./logo.svg";
-import "./App.css";
-import { Button } from "react-bootstrap";
-import { Note } from "./models/note";
+
+import { Button, Col, Container, Row } from "react-bootstrap";
+import { Note as NoteModel } from "./models/note";
+import Note from "./components/Note";
+import styles from "./styles/NotesPage.module.css";
+import stylesUtils from "./styles/utils.module.css";
+import * as NotesApi from "./network/notes_api";
+import AddNoteDialog from "./components/AddNoteDialog";
 
 function App() {
-  const [notes, setNotes] = useState<Note[]>([]);
+  const [notes, setNotes] = useState<NoteModel[]>([]);
+
+  const [showAddNoteDialog, setShowAddNoteDialog] = useState(false);
 
   useEffect(() => {
     async function loadNotes() {
       try {
-        const response = await fetch("/api/notes", {
-          method: "GET",
-        });
-        const notes = await response.json();
+        const notes = await NotesApi.fetchNotes();
         setNotes(notes);
       } catch (error) {
         console.error(error);
@@ -24,9 +28,26 @@ function App() {
   }, []);
 
   return (
-    <div className="App">
-      {JSON.stringify(notes)}
-    </div>
+    <Container>
+      <Button className={`mb-4 ${stylesUtils.blockCenter}`} onClick={() => setShowAddNoteDialog(true)}>
+        Add new note
+      </Button>
+      <Row xs={1} md={2} xl={3} className="g-4">
+        {notes.map((note) => (
+          <Col key={note._id}>
+            <Note note={note} className={styles.note} />
+          </Col>
+        ))}
+      </Row>
+      {showAddNoteDialog && 
+        <AddNoteDialog 
+          onDismiss={() => setShowAddNoteDialog(false)}
+          onNoteSaved={(newNote) => {
+            setNotes([...notes, newNote ])
+            setShowAddNoteDialog(false)
+          }}  
+        />}
+    </Container>
   );
 }
 
